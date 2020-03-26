22f1636380a57e001bc0b6ca6e94bb1a3064e307
vthejaj <vishnutheja@hotmail.com>
2020-03-26 21:18:27 +0530

    linq

---
+++ CSConsoleProject/linq/QueryExpressionsDemo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSConsoleProject.linq
+{
+    class QueryExpressionsDemo
+    {
+        static void Main(string[] args)
+        {
+            var books = Book.GetBooks();
+
+            /*
+            var costlybooks = from b in books
+                              where b.Price > 500
+                              select b;
+
+            foreach(var b in costlybooks)
+                Console.WriteLine(b);
+
+           */
+
+            /*
+            // All books with 20% discount and desc order of price 
+
+            var disbooks = from b in books
+                           orderby b.Price descending
+                           select new { Title = b.Title, Price = b.Price * .80 };
+
+            foreach (var b in disbooks)
+                Console.WriteLine(b);
+
+            */
+            var maxprice = books.Max(b => b.Price);
+            var costlybook = (from b in books
+                              where b.Price == maxprice
+                              select b).First();
+
+            // 
+            var aspbooks = from b in books
+                           where b.Title.Contains("ASP")
+                           select b;
+
+            foreach (var b in aspbooks)
+                Console.WriteLine(b);
+
+
+            var bigtitles = from b in books
+                            where b.Title.Length > 10
+                            orderby b.Price
+                            select b;
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ CSConsoleProject/linq/QueryOperatorsDemo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSConsoleProject.linq
+{
+    class QueryOperatorsDemo
+    {
+        static void Main(string[] args)
+        {
+            var books = Book.GetBooks();
+            /*
+            // Costly books
+            var costly_books = books.Where(b => b.Price > 500);
+            foreach(var b in costly_books)
+                Console.WriteLine(b);
+            */
+
+            /*
+            // Costly books
+            var dis_books = books.Where(b => b.Price > 500)      // selection - filtering
+                                 .Select(b => new { b.Title, 
+                                                    NetPrice = b.Price * 0.80 });  // Projection
+
+            foreach (var b in dis_books)
+                Console.WriteLine($"{b.Title.PadRight(20)} - {b.NetPrice}");
+            */
+
+            /*
+            var sortedbooks = books.OrderBy(b => b.Price).Take(3);
+            foreach(var b in sortedbooks)
+                Console.WriteLine(b);
+            */
+
+            // Book with highest price 
+            var maxprice = books.Max(b => b.Price);
+            var book = books.Where(b => b.Price == maxprice).First();
+            Console.WriteLine(book);
+
+            Console.WriteLine( books.Where(b=> b.Title.Contains("C#"))
+                                    .Sum(b => b.Price));
+
+        }
+    }
+}
